d43dfc94042dcb7f74c37004e42daccc78bba369
DG Theniel <97829510+DgTheniel@users.noreply.github.com>
2025-09-06 21:17:30 +0530

    venue.py

--- event_management/event_management/doctype/venue/venue.py
+++ event_management/event_management/doctype/venue/venue.py
@@ -1,6 +1,3 @@
-# Copyright (c) 2025, Your Name and contributors
-# For license information, please see license.txt
-
 import frappe
 from frappe.model.document import Document
 from frappe import _
